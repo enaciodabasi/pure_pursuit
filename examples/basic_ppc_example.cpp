c90cb9bf1c105250ca9d863824333c08490ddac4
= <=>
2024-07-17 17:14:04 +0300

    Added the main control function of PurePursuitController and an example file.

---
+++ examples/basic_ppc_example.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file basic_ppc_example.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-07-17
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "pure_pursuit/pure_pursuit.hpp"
+#include "point_interpolation/cubic_spline_interpolation.hpp"
+
+int main(int argc, char** argv)
+{
+  if(argc <= 1){
+    return -1;
+  }
+
+
+  
+}
--- include/pure_pursuit/pure_pursuit.hpp
+++ include/pure_pursuit/pure_pursuit.hpp
@@ -1,12 +1,12 @@
 /**
  * @file pure_pursuit.hpp
  * @author your name (you@domain.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2024-06-24
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 
 #ifndef PURE_PURSUIT_HPP_
@@ -18,56 +18,76 @@
 #include <optional>
 #include <vector>
 #include <memory>
+#include <expected>
 
-template<typename T>
-int sgn(T num)
+namespace pure_pursuit
 {
-  return (num < 0 ? -1 : 1);
-}
-
-using Path = std::vector<Point>;
-class PurePursuitController
-{
-  public:
-
-  PurePursuitController() = default;
-
-  ~PurePursuitController() = default;
-
-  struct PurePursuitControllerParams
+  namespace error
   {
-    double lookaheadDistance;
-  };
+    enum class Error
+    {
+      RobotPositionFetchError,
+      LookaheadPointError
+    };
+  }
 
-  std::pair<Path::iterator, Path::iterator> prunePath();
+  namespace
+  {
 
-  bool update();
+    template <typename T>
+    int sgn(T num)
+    {
+      return (num < 0 ? -1 : 1);
+    }
+
+  }
+
+  using Path = std::vector<Point>;
+
+  using VelocityCommand = std::pair<double, double>;
+  class PurePursuitController
+  {
+  public:
+    PurePursuitController() = default;
+
+    ~PurePursuitController() = default;
+
+    struct PurePursuitControllerParams
+    {
+      double lookaheadDistance;
+      double linearVelocity;
+    };
+
+    std::pair<Path::iterator, Path::iterator> prunePath();
+
+    std::expected<VelocityCommand, error::Error> generateVelocityCommand();
 
   private:
+    bool init(PurePursuitControllerParams params);
 
-  bool init(PurePursuitControllerParams params);
-  
-  PurePursuitControllerParams m_ControllerParams;
+    PurePursuitControllerParams m_ControllerParams;
 
-  std::shared_ptr<Path> m_CurrentPath;
+    std::shared_ptr<Path> m_CurrentPath;
 
-  std::weak_ptr<Point> m_RobotPosition;
+    std::weak_ptr<Point> m_RobotPosition;
 
-  Path getCopyOfCurrentPath() const;
+    Path getCopyOfCurrentPath() const;
 
-  /**
-   * @brief first: iterator for the closest point
-   * @brief second: calculated distance
-   * 
-   */
-  std::pair<Path::iterator, double> m_PreviousClosestPoint;
+    /**
+     * @brief first: iterator for the closest point
+     * @brief second: calculated distance
+     *
+     */
+    std::pair<Path::iterator, double> m_PreviousClosestPoint;
 
-  bool findClosestPoint(const Point& robot_position);
+    bool findClosestPoint(const Point &robot_position);
 
-  double calculateCurvature(const Point& robot_position);
+    double calculateCurvature(const Point &lookahead_point);
 
-  std::optional<Point> circleLineSegmentIntercession(const std::pair<Path::iterator, Path::iterator>& segment_points);
+    std::optional<Point> circleLineSegmentIntercession(const std::pair<Path::iterator, Path::iterator> &segment_points);
 
-};
+    VelocityCommand calculateVelocity(const double curvature);
+  };
+} // End of namespace pure_pursuit
 
 #endif // PURE_PURSUIT_HPP_
